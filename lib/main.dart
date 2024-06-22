359d62a237a7ccd20704c70413b63fc3b7d71beb
Zyttal <calz483k@gmail.com>
2024-06-22 15:42:39 +0800

    Add go_router Dependency and Setup - Routes are defined in routes.dart file - screens directory is also initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:gorouter_and_sessions_activity/routes.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MainApp());
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
